b499e1a368c74987e1818dd4d4e0130f018586b7
yanqiang-bpa <yanqiang051@163.com>
2017-05-21 20:59:15 +0800

    Create gas_station.js

---
+++ gas_station.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+var canCompleteCircuit = function(gas, cost) {
+    var eachRemainder = 0;
+    var totalReamainder = 0;
+    var j = -1;
+
+    for(var i=0; i<gas.length; i++){
+    	eachRemainder += gas[i] - cost[i];
+    	totalReamainder += gas[i] - cost[i];
+    	if(eachRemainder < 0){
+    		eachRemainder = 0;
+    		j = i;
+    	}
+    }
+    return totalReamainder >= 0 ? (j + 1) : -1;
+};
