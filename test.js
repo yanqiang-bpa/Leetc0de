604d44cee8fd64b2efa680759cc3abe65b145ac4
yanqiang-bpa <yanqiang051@163.com>
2017-05-28 14:08:38 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,31 @@
-var lst = ["a", "b", 'c'];
-var top = lst.pop();
-console.log(top)
-console.log(lst)
+// var lst = ["a", "b", 'c'];
+// var top = lst.pop();
+// console.log(top)
+// console.log(lst)
+
+// var a=0;
+// var b=6;
+
+// console.log(Math.floor((a+b)/2))
+
+var a = [3,4]
+var binarySearch = function(nums, start, end, target) {
+        if(start == end) {
+            if(target == nums[start])
+                return start;
+            else
+                return -1;
+        }
+        if(start > end) {
+            return -1;
+        }
+        var mid = Math.floor((start + end)/2)
+        if(nums[mid] === target) {
+            return mid;
+        } else if(target > nums[mid]){
+            return binarySearch(nums, mid + 1, end, target);
+        } else {
+            return binarySearch(nums, start, mid - 1, target)
+        }
+    }
+console.log(binarySearch(a, 0, 1, 4))
